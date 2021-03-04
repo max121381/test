1c781906eab984cf24f1b7ef6b46bcf004de4369
max121381 <744529232@qq.com>
2021-03-04 23:06:19 +0800

    create read.c

---
+++ read.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(void)
+{
+	FILE * fp;
+	char ch,filename[10];
+	scanf("%s",filename);
+	if((fp = fopen(filename,"w")) == NULL)
+	{
+		printf("cannot open file\n");
+		exit(0);
+	}
+	ch = getchar();
+	ch = getchar();
+	while(ch != '#')
+	{
+		fputc(ch,fp);
+		putchar(ch);
+		ch = getchar();
+	}
+	fclose(fp);
+}
